ba62e2894b692a6e1fef75e83c15f43010a6867a
mano <kachiraju.mano@gmail.com>
2023-08-10 09:50:04 +0530

    msg

---
+++ src/index.js
@@ -0,0 +1,8 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import App from './TO_Do-Docker/App';
+
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(
+    <App />
+);
